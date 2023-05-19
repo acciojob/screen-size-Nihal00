1011f683a215c66ba240dc2c94d1c56ed56941da
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-19 14:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+document.querySelector('h1').innerHTML = `Width: ${screenWidth} and Height: ${screenHeight}`;
+
